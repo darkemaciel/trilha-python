bb0e9ea2f1aadd3a3c77af71d2f09248639c4a85
Darke Maciel <darke_maciel@hotmail.com>
2026-07-16 16:44:37 -0300

    Adicionando nova pasta de Desafios e o primeiro desafio realizado.

---
+++ Desafios/desafio_1.py
@@ -0,0 +1,7 @@
+# Crie um programa que solicite ao usuário dois números inteiros e exiba a soma desses números.
+
+print("Informe dois valores para somar.")
+input1 = int(input("Digite o primeiro valor: "))
+input2 = int(input("Digite o segundo valor: "))
+result = input1 + input2
+print(f"A soma de {input1} e {input2} é {result}.")
